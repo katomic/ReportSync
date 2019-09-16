dfa808bc5235edbf69ccc7c47607e77a6bf10ec0
katomic <johanfs@gmail.com>
2019-09-16 14:25:36 +0200

    Converted to VS2017, used for SQL 2012-2016.
     Added ability to batch update connections inside reports, and downgrade 2016 reports to 2012.

---
+++ ConvertTo2012.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace ReportSync
+{
+    partial class ConvertTo2012
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ofdConvert = new System.Windows.Forms.OpenFileDialog();
+            this.txtConvertFileName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLoadConvert = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtConvertProgress = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ofdConvert
+            // 
+            this.ofdConvert.DefaultExt = "rdl";
+            this.ofdConvert.Filter = "Report Files|*.rdl";
+            // 
+            // txtConvertFileName
+            // 
+            this.txtConvertFileName.Location = new System.Drawing.Point(15, 37);
+            this.txtConvertFileName.Name = "txtConvertFileName";
+            this.txtConvertFileName.ReadOnly = true;
+            this.txtConvertFileName.Size = new System.Drawing.Size(263, 20);
+            this.txtConvertFileName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select the 2016 .rdl file to be converted";
+            // 
+            // btnLoadConvert
+            // 
+            this.btnLoadConvert.Location = new System.Drawing.Point(281, 36);
+            this.btnLoadConvert.Name = "btnLoadConvert";
+            this.btnLoadConvert.Size = new System.Drawing.Size(24, 23);
+            this.btnLoadConvert.TabIndex = 2;
+            this.btnLoadConvert.Text = "...";
+            this.btnLoadConvert.UseVisualStyleBackColor = true;
+            this.btnLoadConvert.Click += new System.EventHandler(this.btnLoadConvert_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Enabled = false;
+            this.btnConvert.Location = new System.Drawing.Point(67, 65);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(191, 28);
+            this.btnConvert.TabIndex = 4;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txtConvertProgress
+            // 
+            this.txtConvertProgress.Location = new System.Drawing.Point(12, 99);
+            this.txtConvertProgress.Multiline = true;
+            this.txtConvertProgress.Name = "txtConvertProgress";
+            this.txtConvertProgress.ReadOnly = true;
+            this.txtConvertProgress.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtConvertProgress.Size = new System.Drawing.Size(296, 208);
+            this.txtConvertProgress.TabIndex = 7;
+            // 
+            // ConvertTo2012
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 319);
+            this.Controls.Add(this.txtConvertProgress);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.btnLoadConvert);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtConvertFileName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ConvertTo2012";
+            this.Text = "Convert 2016 rdl to 2012";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog ofdConvert;
+        private System.Windows.Forms.TextBox txtConvertFileName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLoadConvert;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtConvertProgress;
+    }
+}
---
+++ ConvertTo2012.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace ReportSync
+{
+    public partial class ConvertTo2012 : Form
+    {
+        string fileName;
+        string sourceFilePath;
+        XDocument reportContent;
+
+        public ConvertTo2012()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnLoadConvert_Click(object sender, EventArgs e)
+        {
+            var res = ofdConvert.ShowDialog();
+
+            if (res == DialogResult.OK)
+            {
+                sourceFilePath = ofdConvert.FileName;
+                btnConvert.Enabled = true;
+                txtConvertFileName.Text = ofdConvert.FileName;
+                fileName = ofdConvert.SafeFileName;
+                txtConvertProgress.Text = $"Loaded {fileName}";
+            }
+
+        }
+
+        private enum ConvertProgress
+        {
+            Loaded,
+            AutoRefresh,
+            DataSources,
+            DataSets,
+            ReportSections,
+            ReportParameters,
+            ReportParametersLayout,
+            EmbeddedImages,
+            Language,
+            ReportUnitType,
+            ReportServerUrl,
+            ReportID,
+            Namespace,
+            Altered,
+            Completed
+        }
+
+
+        private void AddProgress(string line)
+        {
+            txtConvertProgress.Text += Environment.NewLine + line;
+        }
+
+        private void AbortConversion(string line)
+        {
+            txtConvertProgress.Text += Environment.NewLine + line;
+            btnConvert.Enabled = false;
+            MessageBox.Show(line, "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                AddProgress("Starting...");
+                if (!CheckSourceFileIntegrity())
+                {
+                    AbortConversion("Source File Integrity Check failed");
+                    return;
+                }
+
+                Remove2016Nodes();
+                UpdateNamespace();
+                SaveConvertedReport();
+
+            }
+            catch (Exception)
+            {
+                AbortConversion("Conversion failed");
+            }
+
+        }
+
+        private bool CheckSourceFileIntegrity()
+        {
+            var xml = XElement.Load(sourceFilePath);
+
+            XDocument document = XDocument.Parse(xml.ToString());
+
+            var list = xml.Elements().ToList();
+
+            var types = list.Select(x => x.Name.LocalName).ToList();
+
+            if (!types.Contains("ReportID")) return false;
+           
+
+            //Add sections if needed to check
+
+            var sections = new List<string>() { "AutoRefresh",
+                                                "DataSources",
+                                                "DataSets",
+                                                "ReportSections",
+                                                "ReportParameters",
+                                                "ReportParametersLayout",
+                                                "EmbeddedImages",
+                                                "Language",
+                                                "ReportUnitType",
+                                                "ReportServerUrl",
+                                                "ReportID"
+                                                };
+
+            foreach (var section in sections)
+            {
+                if (list.Count(y => y.Name.LocalName == section) == 1)
+                {
+                    AddProgress($"found {section} section");
+                }
+            }
+
+
+            return true;
+        }
+
+        private bool Remove2016Nodes()
+        {
+            try
+            {
+                var xml = XElement.Load(sourceFilePath);
+                xml.Elements().Where(x => x.Name.LocalName == "ReportParametersLayout").Remove();
+
+                reportContent = XDocument.Parse(xml.ToString());
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            return true;
+        }
+
+        private bool UpdateNamespace()
+        {
+            try
+            {
+                var raw = reportContent.ToString();
+                raw = raw.Replace("reporting/2016", "reporting/2010");
+
+
+                reportContent = XDocument.Parse(raw);
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            return true;
+        }
+
+        public void SaveConvertedReport()
+        {
+            try
+            {
+
+                var fileNameWithoutExtension = System.IO.Path.GetFileNameWithoutExtension(sourceFilePath);
+
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.FileName = fileNameWithoutExtension + "_converted_to_2012.rdl";
+                dialog.Title = "Save New Report";
+                dialog.Filter = "Report File|*.rdl";
+                dialog.ShowDialog();
+
+                if (!string.IsNullOrEmpty(dialog.FileName))
+                {
+                    File.WriteAllText(dialog.FileName, reportContent.ToString());
+
+                    MessageBox.Show("Report has been successfully converted.", "Conversion Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    AddProgress($"Conversion Complete: {fileName} created");
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+    }
+}
+
